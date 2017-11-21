b32a34863c1649ba7ae66b4e7be721e84f3f71e9
JAVACLASS <JAVACLASS@15000515cpu.fec.org>
2017-11-21 15:09:51 -0600

    My first java program

---
+++ src/onionpackage/potatobacon.java
@@ -0,0 +1,12 @@
+package onionpackage;
+
+ class potatobacon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("In a medium bowl, combine the beans, oil, salt, and pepper. Roughly mash the mixture with the back of a fork.");
+
+	}
+
+}
